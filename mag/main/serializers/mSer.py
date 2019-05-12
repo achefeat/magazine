55b8fc370e05e203b4e6fe8aa8d05fef013e4543
Balnur <balnur00@mail.ru>
2019-05-12 17:28:41 +0600

    asd

--- mag/main/serializers/mSer.py
+++ mag/main/serializers/mSer.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from main.models import *
+from .ser import *
 
 
 class UserSerializer(serializers.ModelSerializer):
--- mag/main/urls.py
+++ mag/main/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from main import views
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
@@ -31,3 +32,4 @@ if settings.DEBUG:
             'document_root': settings.MEDIA_ROOT,
         }),
     ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
